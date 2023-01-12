15c161a56c3e1a2edae17a9f3da77c9b073e26a6
rumon5h <mdrumonkhan48519@gmail.com>
2023-01-12 16:24:44 +0600

    get user

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const authController = require('../controllers/auth.controllers');
+const express = require("express");
+const authController = require("../controllers/auth.controllers");
 const { verifyToken } = require("../middlewares/verifyToken");
 
 const router = express.Router();
 
-router.route("/signup")
-    .post(authController.signup);
+router.route("/signup").post(authController.signup);
 
+router.get("/me", verifyToken, authController.getMe);
 
 module.exports = router;
