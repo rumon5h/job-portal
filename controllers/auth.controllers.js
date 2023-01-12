aa61ca58d2c73b465941ceb7000e25f679687c17
rumon5h <mdrumonkhan48519@gmail.com>
2023-01-12 16:32:40 +0600

    login

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,4 +1,6 @@
 const { signupService, findUserByEmailService } = require("../services/auth.service");
+const { generateToken } = require("../utils/token");
+
 
 exports.signup = async (req, res) => {
     try {
@@ -26,6 +28,54 @@ exports.getMe = async (req, res) => {
             status: "Success",
             message: 'Successfully retrieved the account',
             data: user
+        })
+    } catch (error) {
+        res.status(500).json({
+            status: "failed",
+            message: "Failed to Login",
+            error: error.message
+        })
+    }
+}
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(401).json({
+                status: "failed",
+                error: "Please provide all your credentials"
+            });
+        }
+
+        const user = await findUserByEmailService(email);
+
+        if (!user) {
+            return res.status(401).json({
+                status: "Failed",
+                message: "No user found please create a account"
+            })
+        }
+
+        const isPasswordValid = user.comparePassword(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(401).json({
+                status: "failed",
+                message: "Password is not valid"
+            })
+        }
+
+        const token = generateToken(user);
+        const { password: pwd, ...others } = user.toObject();
+
+        res.status(200).json({
+            status: "Success",
+            message: "Successfully Login",
+            data: {
+                token
+            }
         })
     } catch (error) {
         res.status(500).json({
